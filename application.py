b242e735016a6286388ea960aeec983cc515b729
mondoman712 <afro.chicken@hotmail.co.uk>
2015-11-28 23:48:36 +0000

    removed second form

--- application.py
+++ application.py
@@ -1,12 +1,3 @@
-'''
-Simple Flask application to test deployment to Amazon Web Services
-Uses Elastic Beanstalk and RDS
-
-Author: Scott Rodkey - rodkeyscott@gmail.com
-
-Step-by-step tutorial: https://medium.com/@rodkey/deploying-a-flask-application-on-aws-a72daba6bb80
-'''
-
 from flask import Flask, render_template, request
 from application import db
 from application.models import Data
@@ -23,7 +14,6 @@ application.secret_key = 'Ludlk5uG5GWWrkPnwfwZuWeVcYuBlwaVc9AS8oUo'
 @application.route('/index', methods=['GET', 'POST'])
 def index():
     form1 = EnterDBInfo(request.form)
-    form2 = RetrieveDBInfo(request.form)
 
     if request.method == 'POST' and form1.validate():
         data_entered = Data(notes=form1.dbNotes.data)
@@ -33,19 +23,8 @@ def index():
             db.session.close()
         except:
             db.session.rollback()
-        return render_template('thanks.html', notes=form1.dbNotes.data,data=data_entered)
-
-    if request.method == 'POST' and form2.validate():
-        try:
-            num_return = int(form2.numRetrieve.data)
-            query_db = Data.query.order_by(Data.id.desc()).limit(num_return)
-            for q in query_db:
-                print(q.notes)
-            db.session.close()
-        except:
-            db.session.rollback()
-        return render_template('results.html', results=query_db,
-                num_return=num_return)
+        return render_template('thanks.html', notes=form1.dbNotes.data,
+                data=data_entered)
 
     try:
         query_dbb = Data.query.order_by(Data.id.desc()).limit(10)
