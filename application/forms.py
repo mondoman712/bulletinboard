b5198d1cfd68f83361da5fa58d7586a94f917fe2
mondoman712 <afro.chicken@hotmail.co.uk>
2015-11-28 19:09:38 +0000

    made forms.py readable

--- application/forms.py
+++ application/forms.py
@@ -2,7 +2,15 @@ from flask.ext.wtf import Form
 from wtforms import TextField, validators
 
 class EnterDBInfo(Form):
-    dbNotes = TextField(label='Items to add to DB', description="db_enter", validators=[validators.required(), validators.Length(min=0, max=128, message=u'Enter 128 characters or less')])    
+    dbNotes = TextField(label='Items to add to DB', 
+            description="db_enter", 
+            validators=[validators.required(), 
+                validators.Length(min=0, max=128, 
+                    message=u'Enter 128 characters or less')])    
 
 class RetrieveDBInfo(Form):
-    numRetrieve = TextField(label='Number of DB Items to Get', description="db_get", validators=[validators.required(), validators.Regexp('^\d{1}$',message=u'Enter a number between 1 and 10')])
+    numRetrieve = TextField(label='Number of DB Items to Get', 
+            description="db_get", 
+            validators=[validators.required(), 
+                validators.Regexp('^\d{1}$',
+                    message=u'Enter a number between 0 and 10')])
