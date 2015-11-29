ba0de3e2eec4bb04ddb52a7de7bc2f81a605d74f
mondoman712 <afro.chicken@hotmail.co.uk>
2015-11-29 01:58:51 +0000

    added posts page

--- application.py
+++ application.py
@@ -9,6 +9,14 @@ application.debug=True
 # change this to your own value
 application.secret_key = 'Ludlk5uG5GWWrkPnwfwZuWeVcYuBlwaVc9AS8oUo'
 
+@application.route('/post/<int:post_id>')
+def show_post(post_id):
+    try:    
+        dbq = Data.query.filter(q.id == post_id)
+        db.session.close()
+    except:
+        db.session.rollback()
+    return render_template('post.html', results=dbq)
 
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/index', methods=['GET', 'POST'])
@@ -17,7 +25,6 @@ def index():
 
     if request.method == 'POST' and form1.validate():
         data_entered = Data(notes=form1.dbNotes.data, username=form1.dbUsername.data)
-        # username = Data(notes=form1.dbUsername.data)
         try:
             db.session.add(data_entered)
             db.session.commit()
