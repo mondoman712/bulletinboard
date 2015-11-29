30e236b7aa2c14ca044e2c5363ec0a56b3bb381d
weitat95 <weitat95@live.com>
2015-11-29 05:55:44 +0000

    check

--- application.py
+++ application.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template, request
 from application import db
 from application.models import Data
 from application.forms import EnterDBInfo, RetrieveDBInfo, LoginForm
-
+from login import LoginManager
 # Elastic Beanstalk initalization
 application = Flask(__name__)
 application.debug=True
 # change this to your own value
 application.secret_key = 'Ludlk5uG5GWWrkPnwfwZuWeVcYuBlwaVc9AS8oUo'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 def get_first(iterable, default=None):
     if iterable:
@@ -15,6 +18,28 @@ def get_first(iterable, default=None):
             return item
     return default
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # next_is_valid should check if the user has valid
+        # permission to access the `next` url
+        if not next_is_valid(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
+"""
 @application.route('/login')
 def login():
     form = LoginForm(request.form)
@@ -27,8 +52,8 @@ def login():
             return abort(400)
 
         return redirect(next or flask.url_for('index'))
-    return render_template('login.html', form1=form)
-
+        return render_template('login.html', form1=form)
+"""
 @application.route('/user/<uname>')
 def show_user(uname):
     try:
